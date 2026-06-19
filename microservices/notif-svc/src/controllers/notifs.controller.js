95c1cc6ae28aa2197c147d16e5728fad2eebf7b9
0wn2886 <boudon.owen@gmail.com>
2026-06-19 16:24:43 +0200

    implement notification retrieval and validation logic

--- microservices/notif-svc/src/controllers/notifs.controller.js
+++ microservices/notif-svc/src/controllers/notifs.controller.js
@@ -1,10 +1,33 @@
-export default (req, res, next) => {
+import prisma from "../config/db..js"
+
+export const getNotifs = async (req, res) => {
     const username = req.get('x-user-username');
     const role = req.get('x-user-role');
 
-    if (username) {
-        return res.status(400).json()
+    if (!username || !role) {
+        return res.status(400).json({ message: 'Missing arguments' });
     }
-    next();
 
-};
+    const allowedTypes = role === 'user'
+  ? ['MENTION', 'LIKE', 'NEW_FOLLOWER', 'COMMENT']
+  : ['MENTION']
+
+    const notifs = await prisma.notification.findMany({
+      where: {
+        recipientUsername: username,
+        type: { in: allowedTypes }
+      },
+      orderBy: { createdAt: 'desc' }
+    })
+
+    return res.json(notifs);
+}
+
+export const markAsRead = async (req, res) => {
+    const username = req.get('x-user-username');
+    const id = req.params("id");
+
+
+}
+export const markAllAsRead = async (req, res) => {}
+export const createNotif = async (req, res) => {}
